ef613be36ea93ee1637bc79d5073c79e16c41e43
Onurcan <33642676+onurcanyilmaz@users.noreply.github.com>
2019-07-16 01:14:19 +0300

    stored procedured
    GET Stored Procedure

--- DerivedTable_TempTableQuery.sql
+++ DerivedTable_TempTableQuery.sql
@@ -1,5 +1,27 @@
-USE TSQLV4
-CREATE TABLE #Temp(
+USE [TSQLV4]
+GO
+/****** Object:  StoredProcedure [dbo].[GetCustom]    Script Date: 16.07.2019 01:11:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[GetCustomQuery]
+	-- Add the parameters for the stored procedure here
+	@id bigint,
+	@country nvarchar(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	CREATE TABLE #Temp(
         id int,
 	    value1 nvarchar(50),
 		value2 nvarchar(50),
@@ -12,7 +34,6 @@ CREATE TABLE #Temp(
 		value9 nvarchar(50),
 		value10 nvarchar(50),
 )
-
 INSERT INTO  #Temp
 SELECT TQ.* FROM (
 SELECT
@@ -34,5 +55,10 @@ LEFT JOIN HR.Employees E ON E.empid = o.empid
 LEFT JOIN Sales.Shippers s ON S.shipperid = O.shipperid
 WHERE  o.custid IN (SELECT TOP 1 custid FROM Sales.Customers sc WHERE sc.contacttitle = 'Owner')
 )TQ
+
+WHERE TQ.OrderId = @id
+ AND TQ.PostalCode > 10076 
+ AND TQ.Country LIKE '%'+ @country+'%' 
 SELECT * FROM #Temp
 DROP TABLE #Temp
+END
